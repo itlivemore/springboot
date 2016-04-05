0b93e9e0c5eee1ddbb141af727ab448b8efaa19f
laigc <lgc13136@126.com>
2016-04-05 19:09:01 +0800

    提交项目

---
+++ springboot-example/.svn/pristine/08/08bc43cc046283d54f66d1e0e003e746be62ac19.svn-base
@@ -0,0 +1,22 @@
+package or.test.springboot.hook;
+
+/**
+ * @Title ShutdownHookThread.java
+ * @Package: com.gil.springbootjar
+ * @Description: 关闭进程时执行的线程
+ *
+ * @Author: laigc
+ * @Date: 2016年3月28日 下午2:40:17
+ *
+ *        Copyright @ 2016 Corpration Name
+ * 
+ */
+public class ShutdownHookThread implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("要被kill......");
+		System.exit(0);
+	}
+
+}
---
+++ springboot-example/src/main/java/or/test/springboot/controller/TestController.java
@@ -0,0 +1,27 @@
+package or.test.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Title TestController.java
+ * @Package: com.gil.springbootjar.controller
+ * @Description: 测试用的Controller
+ *
+ * @Author: lgc
+ * @Date: 2016年4月5日 下午4:40:49
+ *
+ *        Copyright @ 2016 Corpration Name
+ * 
+ */
+@Controller
+@RequestMapping("/")
+public class TestController {
+	@RequestMapping("/hello")
+	@ResponseBody()
+	String home() {
+		return "Hello World!";
+	}
+
+}
